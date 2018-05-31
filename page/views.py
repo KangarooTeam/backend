6f2c0866e869e084eab8d0b089d3881759fe7299
KangarooTeam <kangarooteam.py@gmail.com>
2018-05-31 21:39:33 +0300

    пагинация

--- page/views.py
+++ page/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Articles
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 def index(request):
     posts = Articles.objects.all()
+    paginator = Paginator(posts, 3)
+    page = request.GET.get('page')
+
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'homepage/wrapper.html', {'posts': posts})
 
 
