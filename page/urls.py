b4966a167948ef2d9680b6769d74f30793882170
Max <fando97@mail.ru>
2018-06-02 22:07:45 +0300

    Сделал форму и страницу регистрации

--- page/urls.py
+++ page/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
      url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
      url(r'^$', views.index, name='index'),
+     url(r'^register/', views.RegisterFormView.as_view())
 ]
