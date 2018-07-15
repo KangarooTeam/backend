ce112f4828d9e0f342c645f2143032263ff844f9
KangarooTeam <kangarooteam.py@gmail.com>
2018-07-15 21:43:07 +0300

    поиск пагинация 30 результатов

--- page/views.py
+++ page/views.py
@@ -20,8 +20,8 @@ def index(request, tag_slug=None):
 
     if len(posts) > 0:
         last_post = posts[0]
-        if len(posts) >= 30:
-            paginator = Paginator(posts, 30)
+        if len(posts) >= 5:
+            paginator = Paginator(posts, 5)
         else:
             paginator = Paginator(posts, len(posts))
         page = request.GET.get('page')
@@ -35,39 +35,45 @@ def index(request, tag_slug=None):
             posts = paginator.page(paginator.num_pages)
     return render(request, 'homepage/wrapper.html', locals(), {'posts': posts, "last_post": last_post})
 
+
 def post_detail(request, pk):
     post = get_object_or_404(Articles, pk=pk)
     return render(request, 'homepage/post_detail.html', {'post': post})
 
-def search(request):
-     q = request.GET['q']
-     res = [i for i in q.split()]
-     cash = []
-     result = False
-     if q:
-         result = []
-         for j in res:
-             if j not in cash:
-                 cash.append(j)
-                 result.append(Articles.objects.filter(
-                     Q(title__icontains=j)|
-                     Q(body__icontains=j)))
-     if len(result) >= 1:
-         paginator = Paginator(result, 1)
-     else:
-         paginator = Paginator(result, len(result))
-     page = request.GET.get('q')
-     try:
-         result = paginator.page(page)
-     except PageNotAnInteger:
-         # If page is not an integer, deliver first page.
-         result = paginator.page(1)
-     except EmptyPage:
-         # If page is out of range (e.g. 9999), deliver last page of results.
-         result = paginator.page(paginator.num_pages)
 
-     return render_to_response('homepage/search.html', locals(),
-                               {"result": result, 'q': q})
+def search(request):
+    q = request.GET['q']
+    res = [i for i in q.split()]
+    cash = []
+    result = []
+    add_res = []
+    if q:
+        result = []
+        for j in res:
+            if j not in cash:
+                cash.append(j)
+                result.append(Articles.objects.filter(
+                    Q(title__icontains=j) |
+                    Q(body__icontains=j)))
+
+    if len(result) > 0:
+        add_res = [i for i in result[-1]]
+        add_res = add_res[-1]
+        if len(result) >= 30:
+            paginator = Paginator(result, 30)
+        else:
+            paginator = Paginator(result, len(result))
+        page = request.GET.get('page')
+        try:
+            result = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            result = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            result = paginator.page(paginator.num_pages)
+    return render_to_response('homepage/search.html',
+                              {"result": result, "add_res": add_res, 'q': q})
 
 
 def handler404(request):
@@ -75,13 +81,16 @@ def handler404(request):
         render(request, "errors/404.html")
     )
 
+
 class Information():
     def developers(request):
         return render(request, 'homepage/developers.html')
 
+
 class Contacts():
     def contacts(request):
         return render(request, 'homepage/contacts.html')
+
 
 def search_list(request):
     return render(request, 'homepage/search.html')
@@ -90,9 +99,7 @@ def search_list(request):
 def show_genres(request):
     return render(request, "homepage/category.html", {'genres': Genre.objects.all()})
 
+
 def move_category(request, id):
     target = get_object_or_404(Genre, pk=id)
     return render(request, 'homepage/category.html', {'target': target})
-
-
-
