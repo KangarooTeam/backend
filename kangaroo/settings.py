630cc1b1580fe2eeb3e87d644eab3f867c83568b
KangarooTeam <kangarooteam.py@gmail.com>
2018-07-17 20:42:06 +0300

    go

--- kangaroo/settings.py
+++ kangaroo/settings.py
@@ -95,7 +95,7 @@ if not DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': 'mydatabase'
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 
